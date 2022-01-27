267d1c6e32c6b7a837945bd479555cf47f21ce59
dirkarnez <smalldirkalex@gmail.com>
2022-01-27 22:23:59 +0800

    - working proof of concept

--- main.go
+++ main.go
@@ -17,6 +17,9 @@ func main() {
 
 	var TARGET_OWNER = os.Getenv("TARGET_OWNER")
 	var TARGET_REPOSITORY = os.Getenv("TARGET_REPOSITORY")
+	var TARGET_LIBRARY = os.Getenv("TARGET_LIBRARY")
+	var TARGET_TAG = os.Getenv("TARGET_TAG")
+	var TARGET_COMPILER = os.Getenv("TARGET_COMPILER")
 
 	tp := github.BasicAuthTransport{
 		Username: strings.TrimSpace(userName),
@@ -24,31 +27,33 @@ func main() {
 	}
 
 	client := github.NewClient(tp.Client())
-	release, resp, err := client.Repositories.GetReleaseByTag(context.Background(), TARGET_OWNER, TARGET_REPOSITORY, "2018-07-31")
+	release, resp, err := client.Repositories.GetReleaseByTag(context.Background(), TARGET_OWNER, TARGET_REPOSITORY, TARGET_TAG)
 	if err != nil {
 		fmt.Printf("Repositories.GetReleaseByTag returned error: %v\n%v\n", err, resp.Body)
 	}
 
 	if len(release.Assets) < 1 {
-		fmt.Printf("NO Assets")
+		fmt.Printf("No Assets")
 		return
 	}
 
-	var toDownloadAsset *github.ReleaseAsset = nil
+	// for example: sfml-v2.5.1-mingw.zip
+	targetAssetName := fmt.Sprintf("%s-%s-%s.zip", TARGET_LIBRARY, TARGET_TAG, TARGET_COMPILER)
+	var assetToDownload *github.ReleaseAsset = nil
 	for _, asset := range release.Assets {
-		if asset.GetName() == "x86_64-full.tar.bz2" {
-			toDownloadAsset = asset
+		if asset.GetName() == targetAssetName {
+			assetToDownload = asset
 		}
 	}
 
-	readCloser, _, err := client.Repositories.DownloadReleaseAsset(context.Background(), TARGET_OWNER, TARGET_REPOSITORY, toDownloadAsset.GetID(), http.DefaultClient)
+	readCloser, _, err := client.Repositories.DownloadReleaseAsset(context.Background(), TARGET_OWNER, TARGET_REPOSITORY, assetToDownload.GetID(), http.DefaultClient)
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		return
 	}
 	defer readCloser.Close()
 
-	outFile, err := os.Create(toDownloadAsset.GetName())
+	outFile, err := os.Create(assetToDownload.GetName())
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		return
--- run.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-set USER_NAME=dirkarnez
-set PASSWORD=????
-
-set TARGET_OWNER=Khang-NT
-set TARGET_REPOSITORY=ffmpeg-binary-android
-
-cpk.exe
-pause
